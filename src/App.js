a9435c7316c5fc11a034cb647bfab43de9cde799
sachingaykhe1209 <sachingaykhe1209@gmail.com>
2021-01-31 15:10:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div>
+            Welcome to the react todo-app.
+          </div><br/>
+          <button className="btn btn-primary">
+             Click to open Dashboard.
+          </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
